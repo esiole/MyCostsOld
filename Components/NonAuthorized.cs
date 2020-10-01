191efc104dd1bb4d2c78bf1bcaa4ef92c5f94957
Олег Есиков <esikov.oleg@mail.ru>
2020-10-01 11:26:40 +0300

    Ограничение функционала для не авторизованных

---
+++ Components/NonAuthorized.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace MyCosts
+{
+    [ViewComponent]
+    public class NonAuthorized
+    {
+        public IViewComponentResult Invoke()
+        {
+            return new HtmlContentViewComponentResult(
+                new HtmlString($"<div><p><b>Авторизуйтесь, чтобы использовать приложение!</b></p></div>")
+            );
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MyCosts.Models;
 
 namespace MyCosts.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -21,6 +23,7 @@ namespace MyCosts.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
