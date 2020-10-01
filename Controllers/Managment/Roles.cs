de84452a58ddfaace5d53238d93085f52d306708
Олег Есиков <esikov.oleg@mail.ru>
2020-10-01 20:41:10 +0300

    Добавление ролей

---
+++ Controllers/Managment/Roles.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCosts.Controllers.Managment
+{
+    [Authorize]
+    public class Roles : Controller
+    {
+        private RoleManager<IdentityRole> roleManager;
+
+        public Roles(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await roleManager.Roles.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                var result = await roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(name);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                await roleManager.DeleteAsync(role);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Controllers/Managment/Users.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCosts.Controllers.Managment
+{
+    [Authorize]
+    public class Users : Controller
+    {
+        private UserManager<User> userManager;
+        private RoleManager<IdentityRole> roleManager;
+
+        public Users(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await userManager.Users.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Delete(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                await userManager.DeleteAsync(user);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Roles(string userId)
+        {
+            var user = await userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                var userRoles = await userManager.GetRolesAsync(user);
+                var allRoles = await roleManager.Roles.ToListAsync();
+                var model = new ChangeRole
+                {
+                    UserId = user.Id,
+                    UserEmail = user.Email,
+                    UserRoles = userRoles,
+                    AllRoles = allRoles
+                };
+                return View(model);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Roles(string userId, List<string> roles)
+        {
+            var user = await userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                var userRoles = await userManager.GetRolesAsync(user);
+                var allRoles = await roleManager.Roles.ToListAsync();
+                var addedRoles = roles.Except(userRoles);
+                var removedRoles = userRoles.Except(roles);
+                await userManager.AddToRolesAsync(user, addedRoles);
+                await userManager.RemoveFromRolesAsync(user, removedRoles);
+                return RedirectToAction("Index");
+            }
+            return NotFound();
+        }
+    }
+}
