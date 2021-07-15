a43be66ae171b1d87882307802f136f1f10f4737
Oleg Esikov <esikov.oleg@mail.ru>
2021-07-15 09:17:06 +0300

    Добавление viewmodel для пагинации

---
+++ ViewModels/Pagination.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace MyCosts.ViewModels
+{
+    public class Pagination<T> where T : class
+    {
+        public IEnumerable<T> Records { get; set; }
+        public int Page { get; set; }
+        public int PerPage { get; set; }
+        public int CountRecords { get; set; }
+    }
+}
