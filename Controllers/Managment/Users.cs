c667262828d1d750941c27716b0dc328cabd69e7
Oleg Esikov <esikov.oleg@mail.ru>
2021-07-15 09:57:15 +0300

    Добавлена пагинация для страницы с пользователями

--- Controllers/Managment/Users.cs
+++ Controllers/Managment/Users.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyCosts.Models;
+using MyCosts.ViewModels;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace MyCosts.Controllers.Managment
     [Authorize(Roles = "admin")]
     public class Users : Controller
     {
+        private const int SizePage = 10;
         private UserManager<User> userManager;
         private RoleManager<IdentityRole> roleManager;
 
@@ -22,9 +24,19 @@ namespace MyCosts.Controllers.Managment
         }
 
         [HttpGet]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int page = 1)
         {
-            return View(await userManager.Users.OrderBy(u => u.Email).ToListAsync());
+            var skip = (page - 1) * SizePage;
+            var query = userManager.Users.OrderBy(u => u.Email);
+            var users = await query.Skip(skip).Take(SizePage).ToListAsync();
+            var totalCount = await userManager.Users.CountAsync();
+            return View(new Pagination<User>
+            {
+                Records = users,
+                Page = page,
+                PerPage = SizePage,
+                CountRecords = totalCount
+            });
         }
 
         [HttpPost]
