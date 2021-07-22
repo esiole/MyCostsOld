a0183d0368b3c93a4956079883896ce626ab1de4
Oleg Esikov <esikov.oleg@mail.ru>
2021-07-22 09:41:02 +0300

    Добавлено подключение пакета для отрисовки диаграмм

--- Controllers/Statistics.cs
+++ Controllers/Statistics.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using ChartJSCore.Helpers;
+using ChartJSCore.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace MyCosts.Controllers
 {
@@ -9,6 +12,42 @@ namespace MyCosts.Controllers
         [HttpGet]
         public IActionResult Index()
         {
+            LineDataset dataset = new()
+            {
+                Label = "My First dataset",
+                Data = new List<double?> { 65, 59, 80, 81, 56, 55, 40 },
+                Fill = "false",
+                LineTension = 0.1,
+                BackgroundColor = ChartColor.FromRgba(75, 192, 192, 0.4),
+                BorderColor = ChartColor.FromRgb(75, 192, 192),
+                BorderCapStyle = "butt",
+                BorderDash = new List<int> { },
+                BorderDashOffset = 0.0,
+                BorderJoinStyle = "miter",
+                PointBorderColor = new List<ChartColor> { ChartColor.FromRgb(75, 192, 192) },
+                PointBackgroundColor = new List<ChartColor> { ChartColor.FromHexString("#ffffff") },
+                PointBorderWidth = new List<int> { 1 },
+                PointHoverRadius = new List<int> { 5 },
+                PointHoverBackgroundColor = new List<ChartColor> { ChartColor.FromRgb(75, 192, 192) },
+                PointHoverBorderColor = new List<ChartColor> { ChartColor.FromRgb(220, 220, 220) },
+                PointHoverBorderWidth = new List<int> { 2 },
+                PointRadius = new List<int> { 1 },
+                PointHitRadius = new List<int> { 10 },
+                SpanGaps = false
+            };
+
+            ChartJSCore.Models.Data data = new()
+            {
+                Labels = new List<string>() { "January", "February", "March", "April", "May", "June", "July" },
+                Datasets = new List<Dataset>()
+            };
+            data.Datasets.Add(dataset);
+
+            ViewData["chart"] = new Chart
+            {
+                Type = Enums.ChartType.Line,
+                Data = data
+            };
 
             return View();
         }
