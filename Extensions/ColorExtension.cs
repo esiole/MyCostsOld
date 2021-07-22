ab4acbc341c6b47e8b607a0cb0213c0d119408a4
Oleg Esikov <esikov.oleg@mail.ru>
2021-07-22 11:24:23 +0300

    Добавлен метод расширения для вывода цвета в 16 формат

---
+++ Extensions/ColorExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace MyCosts.Extensions
+{
+    public static class ColorExtension
+    {
+        public static string ToHexString(this Color color)
+        {
+            return "#" + color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
+        }
+    }
+}
