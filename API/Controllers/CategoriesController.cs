68c503f00ffa14d6a4a3efb70ff53b77ecc3b353
Oleg Esikov <esikov.oleg@mail.ru>
2022-01-31 23:10:02 +0300

    refactor: структура контроллеров
    типовая структура для контроллеров с основными действиями

---
+++ API/Controllers/CategoriesController.cs
@@ -0,0 +1,35 @@
+﻿namespace MyCosts.API.Controllers;
+
+[Route("api/v2.0/[controller]")]
+[ApiController]
+public class CategoriesController : ControllerBase
+{
+    public CategoriesController()
+    {
+
+    }
+
+    [HttpPost]
+    public IActionResult Add()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Remove([FromRoute] int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut]
+    public IActionResult Update()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿namespace MyCosts.API.Controllers;
+
+[Route("api/v2.0/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly IProductsService _productsService;
+
+    public ProductsController(IProductsService productsService)
+    {
+        _productsService = productsService;
+    }
+
+    [HttpPost]
+    public IActionResult Add()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        List<Product> products = _productsService.Get();
+        return Ok(products);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Remove([FromRoute] int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut]
+    public IActionResult Update()
+    {
+        throw new NotImplementedException();
+    }
+}
